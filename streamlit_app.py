69f142d892df7591510f0c5139a64585bd2da17d
James Jian <49461855+jamesjian007@users.noreply.github.com>
2023-08-28 20:19:53 +1000

    new cv

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,7 @@ SOCIAL_MEDIA = {
     "LinkedIn": "https://linkedin.com",
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Sales Dashboard - Comparing sales across three stores",
 }
 
 
